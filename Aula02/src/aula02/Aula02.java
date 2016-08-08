0d24475251e86e801f76b02da80b7ab4a2115da2
Welton <weltonvaz@gmail.com>
2016-08-08 14:31:32 -0300

    second commit

--- Aula02/src/aula02/Aula02.java
+++ Aula02/src/aula02/Aula02.java
@@ -5,15 +5,7 @@ public class Aula02 {
         c1.cor = "azul";
         c1.ponta = 0.5f;
         c1.tampada = false;
-        
-                
-        
-                
-    }
-
-   
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    } 
+    
     
 }
