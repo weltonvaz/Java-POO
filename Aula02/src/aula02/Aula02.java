b06dc4292b7ed523219decf4f5ea3dda7fd098f1
Welton <weltonvaz@gmail.com>
2016-08-08 14:23:58 -0300

    second commit

--- Aula02/src/aula02/Aula02.java
+++ Aula02/src/aula02/Aula02.java
@@ -1,19 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aula02;
-
-/**
- *
- * @author welton
- */
 public class Aula02 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.cor = "azul";
+        c1.ponta = 0.5f;
+        c1.tampada = false;
+        
+                
+        
+                
+    }
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         // TODO code application logic here
     }
