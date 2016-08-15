50810c375ab8e752ed3932992ed61231a7c37087
Welton Vaz <weltonvaz@gmail.com>
2016-08-15 19:59:01 -0300

    Comitando de Casa em 15/08/2016

--- Aula02/src/aula02/Aula02.java
+++ Aula02/src/aula02/Aula02.java
@@ -2,6 +2,10 @@ package aula02;
 public class Aula02 {
     public static void main(String[] args) {
         Caneta c1 = new Caneta();
+        
+        c1.modelo = "BIC Cristal";
+        c1.cor = "Azul";
+        c1.carga = 80;
         c1.status();
         
     } 
